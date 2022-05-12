e99f77f077f34f436dc1960aacae1efca02242b0
Oded <odedatias8115@gmail.com>
2022-05-12 22:18:32 +0300

    Adding ApoitmentNewHoa design

--- src/layout/AppointmentNewHOA.jsx
+++ src/layout/AppointmentNewHOA.jsx
@@ -1,32 +1,49 @@
 import React from 'react';
+import BackButton from '../features/BackButton';
 
 function AppointmentNewHOA() {
 	return (
-		<div className='wrapper'>
-			<div className='formContainer'>
-				<span className='formHeading'></span>
-				<form action=''>
-					{/* Input 1 */}
-					<div className='input-group'>
-						<i class='fa-brands fa-facebook'></i>
-						<input type='text' placeholder='Username...' required />
-						<span className='bar'></span>
-					</div>
-					{/* Input 2 */}
-					<div className='input-group'>
-						<i class='fa-brands fa-facebook'></i>
-						<input type='text' placeholder='Username...' required />
-						<span className='bar'></span>
-					</div>
-					{/* Input 3 */}
-					<div className='input-group'>
-						<i class='fa-brands fa-facebook'></i>
-						<input type='text' placeholder='Username...' required />
-						<span className='bar'></span>
-					</div>
-				</form>
+		<>
+			<div className='wrapper'>
+				<div className='formContainer'>
+					<span className='formHeading'>מינוי יו"ר ועד בית</span>
+					<form action=''>
+						{/* Input 1 */}
+						<div className='input-group'>
+							<i class='fa-solid fa-user'></i>
+							<input type='text' placeholder=' שם משתמש' required />
+							<span className='bar'></span>
+						</div>
+						{/* Input 2 */}
+						<div className='input-group'>
+							<i class='fa-solid fa-lock'></i>
+							<input type='password' placeholder=' סיסמא' required />
+							<span className='bar'></span>
+						</div>
+						{/* Input 3 */}
+						<div className='input-group'>
+							<i class='fa-solid fa-building'></i>
+							<input
+								type='number'
+								placeholder=' מספר בניין'
+								required
+								max={18}
+								min={1}
+							/>
+							<span className='bar'></span>
+						</div>
+						{/* Input 1 */}
+						<div className='input-group'>
+							<button>
+								אישור
+								{/* <i class="fa-solid fa-paper-plane-top"></i>	 */}
+							</button>
+						</div>
+					</form>
+				</div>
 			</div>
-		</div>
+			<BackButton />
+		</>
 	);
 }
 
