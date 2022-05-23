7b9f36bdd37748ef43d64918bb3d6781f26d01ef
Oded <odedatias8115@gmail.com>
2022-05-23 19:58:05 +0300

    Removing incorrect clicks

--- src/layout/ChooseBuilding.jsx
+++ src/layout/ChooseBuilding.jsx
@@ -4,19 +4,23 @@ import buildingImg from '../images/buildingImg.jpg';
 import Option from '../features/Option';
 import BackButton from '../features/BackButton';
 
-
 function ChooseBuilding() {
 	let [options, optionName] = [[], ''];
 	for (let i = 1; i <= 18; i++) {
 		let option_Name = 'בניין ' + i;
 		// options[i] = <Option optionName={option_Name} imgAdd={buildingImg} />;
-		options[i]=<Link to='/BuildingOperation' className='link'> <Option optionName={option_Name} imgAdd={buildingImg} /></Link>
+		options[i] = (
+			<Link to='/BuildingOperation' className='link'>
+				{' '}
+				<Option optionName={option_Name} imgAdd={buildingImg} />
+			</Link>
+		);
 	}
 	return (
 		<div className='pageTemplate'>
 			<h1>בחר בניין</h1>
 			<div className='optionsContainer'>{options}</div>
-			<Link to='/ManagerHomePage' className='link'>
+			<Link to='/ManagerHomePage' className='link link-button'>
 				<BackButton />
 			</Link>
 		</div>
