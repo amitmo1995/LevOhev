425b8bb622b13e39b66d5af73ce95b485788549a
david chen <dach755@gmail.com>
2022-05-25 05:14:46 +0300

    get the information and fill the table

--- src/layout/BuildingExpenses.jsx
+++ src/layout/BuildingExpenses.jsx
@@ -1,43 +1,162 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BackButton from '../features/BackButton';
 import { Link } from 'react-router-dom';
+import {firestore} from '../firebase/firebase';
+import {where,doc,setDoc,getDoc, addDoc,add, collection,onSnapshot, query, getDocs} from 'firebase/firestore';
+import { async } from '@firebase/util';
+
+let sortByDate=function(date_1,date_2){
+
+    date_1=date_1.split('-');
+    date_2=date_2.split('-');
+
+    //compare year
+    date_1[0]=parseInt(date_1[0]);
+    date_2[0]=parseInt(date_2[0]);
+    if( date_1[0]>date_2[0]){
+        return 1;
+    }else if(date_2[0]> date_1[0]){
+        return -1;
+    }
+
+    //compare mounth
+    date_1[1]=parseInt(date_1[1]);
+    date_2[1]=parseInt(date_2[1]);
+    if( date_1[1]>date_2[1]){
+        return 1;
+    }else if(date_2[1]> date_1[1]){
+        return -1;
+    }
+
+    //compare day
+    date_1[2]=parseInt(date_1[2]);
+    date_2[2]=parseInt(date_2[2]);
+    if( date_1[2]>date_2[2]){
+        return 1;
+    }else if(date_2[2]> date_1[2]){
+        return -1;
+    }
+    return 0;
+}
 
 function BuildingExpenses() {
+    const [loding,setLoding]=useState(true);
+    const [children,setChildren]=useState(<div>ijdfoijoirfjo</div>);
+    
+    
+    let buildingExpens={};
+    let keys="hhhhhhhhhhhhhhhhhhhhhhhhh";
+
+    ///let children=<div>ijdfoijoirfjo</div>;
+    async function getData(){   
+    ///get buildingId from the url param 
+    const buildingId="1";
+    //get the building expense
+    try{
+        //get the apartment monthly payment
+        let collectionRef=collection(firestore,'monthly_payment');
+        let apartQuery= query(collectionRef,where("building","==",buildingId));
+        let apartQurySnapshot= await getDocs(apartQuery);
+        apartQurySnapshot.forEach(doc=>{
+            buildingExpens[doc.id]=doc.data();
+            buildingExpens[doc.id]["reason"]="תשלום דירה "+ buildingExpens[doc.id]["apartment"];
+            //add color to the table row
+            buildingExpens[doc.id]["color"]="green";
+        });
+
+        //get the apartment HOA expanse
+        collectionRef=collection(firestore,'HOA_expense');
+        apartQuery= query(collectionRef,where("building","==",buildingId));
+        apartQurySnapshot= await getDocs(apartQuery);
+        apartQurySnapshot.forEach(doc=>{
+            buildingExpens[doc.id]=doc.data();
+            //add negative sign to the expense
+            buildingExpens[doc.id]["amount"]="- "+ String(buildingExpens[doc.id]["amount"]);
+            //add color to the table row
+            buildingExpens[doc.id]["color"]="red";
+        });
+        //get the grant payment
+        collectionRef=collection(firestore,'grant_payment');
+        apartQuery= query(collectionRef,where("building","==",buildingId));
+        apartQurySnapshot= await getDocs(apartQuery);
+        apartQurySnapshot.forEach(doc=>{
+            buildingExpens[doc.id]=doc.data();
+            buildingExpens[doc.id]["reason"]="מענק מהעיריה לוועד הבית";
+            //add color to the table row
+            buildingExpens[doc.id]["color"]="green";
+        });
+
+
+
+        keys=Object.keys(buildingExpens);
+        //sort the result by date
+        keys.sort((key1,key2)=>{return sortByDate(buildingExpens[key1]["date"],buildingExpens[key2]["date"]);});
+        //set the result in descending order
+        keys.reverse();
+        console.log("map");
+        let temp=keys.map(key=>{
+            console.log(buildingExpens[key]["date"]);
+            console.log(buildingExpens[key]["amount"]);
+            return (
+                <tr>
+                     <td>{buildingExpens[key]["amount"]}</td>
+                     <td>{buildingExpens[key]["date"]}</td>
+                     <td>{buildingExpens[key]["reason"]}</td>
+                </tr>
+            );
+          });
+          console.log(" end map");
+          console.log(children);
+          setChildren(temp);
+          console.log(children);
+          setLoding(false);
+    }catch{
+        console.log("error on apartment id Query");
+    }
+    }
+
+    useEffect(()=>{getData();},[]);
+    //useEffect(()=>{},[children])
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    if(loding){
+        return <h1>loding ... </h1>;
+    }
+
+
 	return (
 		<>
 			<div className='tableData'>
                 <h1>מעקב הוצאות/הכנסות</h1>
-            <table>
-                <tr>
-                    <th>סכום</th>
-                    <th>תאריך</th>
-                    <th>תיאור</th>
-                    </tr>
-                    <tr>
-                        <td>100</td>
-                        <td>09.11.22</td>
-                        <td>גנן</td>
-                        </tr>
+                <table>
+                    <thead>
                         <tr>
-                            <td>150</td>
-                            <td>01.02.22</td>
-                            <td>ניקיון</td>
-                             </tr>
-                             <tr>
-                                 <td>500</td>
-                                  <td>04.02.21</td>
-                                  <td>צבע בניין</td>
-                                  </tr>
-                                  <tr>
-                                      <td>250</td>
-                                      <td>20.08.22</td>
-                                      <td>פעילות לילדים</td>
-                                      </tr>
-                                      </table>
-                                      <Link to='/FinancialManagement' className='link'>
-                                          <BackButton />
-                                          </Link>
-                                      </div>
+                            <th>סכום</th>
+                            <th>תאריך</th>
+                            <th>תיאור</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {children}
+                    </tbody>
+                </table>
+                <Link to='/FinancialManagement' className='link'>
+                    <BackButton />
+                </Link>
+             </div>
 		</>
 	);
  }
