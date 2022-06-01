ec772262101e8a0e1f70b140b66c209901ed6151
benaa1995 <ben.alaluf2@gmail.com>
2022-06-01 20:30:48 +0300

    balance

--- src/layout/BuildingExpenses.jsx
+++ src/layout/BuildingExpenses.jsx
@@ -5,6 +5,49 @@ import {firestore} from '../firebase/firebase';
 import {where, collection, query, getDocs} from 'firebase/firestore';
 import HomePageButton from '../features/HomePageButton'
 
+let getBuildingBalance=async function(setBalance,buildingId){
+
+	let buildingExpens={};
+let keys="";
+
+//get the building expense
+try{
+	//get the apartment monthly payment
+	let collectionRef=collection(firestore,'monthly_payment');
+	let apartQuery= query(collectionRef,where("building","==",buildingId));
+	let apartQurySnapshot= await getDocs(apartQuery);
+	apartQurySnapshot.forEach(doc=>{
+		buildingExpens[doc.id]=parseFloat(doc.data().amount);
+	});
+
+	//get the apartment HOA expanse
+	collectionRef=collection(firestore,'HOA_expense');
+	apartQuery= query(collectionRef,where("building","==",buildingId));
+	apartQurySnapshot= await getDocs(apartQuery);
+	apartQurySnapshot.forEach(doc=>{
+		//add negative sign to the expense
+		buildingExpens[doc.id]=(-parseFloat(doc.data().amount));
+	});
+	//get the grant payment
+	collectionRef=collection(firestore,'grant_payment');
+	apartQuery= query(collectionRef,where("building","==",buildingId));
+	apartQurySnapshot= await getDocs(apartQuery);
+	apartQurySnapshot.forEach(doc=>{
+		buildingExpens[doc.id]=parseFloat(doc.data().amount);
+	});
+	console.log(buildingExpens)
+
+
+   keys=Object.keys(buildingExpens);
+   let temp=keys.reduce((sum,currentKey)=>sum+buildingExpens[currentKey],0);
+   console.log(temp);
+   setBalance(temp); 
+
+}catch{
+	console.log("error on apartment id Query");
+}
+
+}
 
 let sortByDate=function(date_1,date_2){
 
@@ -40,7 +83,7 @@ let sortByDate=function(date_1,date_2){
     return 0;
 }
 
-function BuildingExpenses() {
+function BuildingExpenses(props) {
 
     const params= useParams();
     let routBack="/FinancialManagement/"+params.building_id;
@@ -117,6 +160,8 @@ function BuildingExpenses() {
         console.log("error on apartment id Query");
     }
     }
+    const [balance,setBalance]=useState("");
+	useEffect(()=>{getBuildingBalance(setBalance,params.building_id);},[]);
 
     useEffect(()=>{getData();},[]);
 
@@ -124,7 +169,7 @@ function BuildingExpenses() {
     if(loding){
         return <h1>loding ... </h1>;
     }
-
+   
 
 	return (
 		<>
@@ -142,6 +187,16 @@ function BuildingExpenses() {
                     <tbody>
                         {children}
                     </tbody>
+                </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>יתרה בחשבון</th>
+                        </tr>
+                        <tr>
+                        <th>{balance}</th>
+                        </tr>
+                </thead>
                 </table>
                 <Link to={routBack} className='link'>
                     <BackButton />
