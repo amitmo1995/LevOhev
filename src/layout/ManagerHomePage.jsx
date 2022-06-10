52c9ba2fdaa2b02e24b7d55048ae5268e37506ab
Amit Moshe <amitmo1995@gmail.com>
2022-06-11 00:24:10 +0300

    managerHomePageDesign

--- src/layout/ManagerHomePage.jsx
+++ src/layout/ManagerHomePage.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import meetingImg from '../images/meetingImg.jpg';
 import buildingManageImg from '../images/buildingManageImg.jpg';
 import appointmentNewHOAImg from '../images/appointmentNewHOAImg.jpg';
+import MeetingSummaryImg from '../images/MeetingSummaryImg.jpg';
 import { useAuth } from '../firebase/firebase';
 import { LogoutButton } from '../features/LogoutButton';
 
@@ -12,16 +13,18 @@ function ManagerHomePage(props) {
 		<Link to='/ChooseBuilding' className='link'> <Option optionName='ניהול בניין' imgAdd={buildingManageImg} /> </Link>,
 		<Link to='/MeetingManagement' className='link'> <Option optionName='פגישות' imgAdd={meetingImg} /> </Link>,
 		<Link to='/AppointmentNewHOA' className='link'> <Option optionName='הוספת משתמש' imgAdd={appointmentNewHOAImg}  /> </Link>,
-		<Link to='/SetMessage' className='link'> <Option optionName='שלח הודעה לוועדי בתים' imgAdd={appointmentNewHOAImg}  /> </Link>
+		<Link to='/SetMessage' className='link'> <Option optionName='שלח הודעה לוועדי בתים' imgAdd={MeetingSummaryImg}  /> </Link>
 	];
 
 	return (
+		<div className='hoaHome'>
 		<div className='pageTemplate'>
 			<h1> {JSON.parse(localStorage.getItem("userConnected")).data.name} שלום</h1>
 			<div className='optionsContainer'>{options}</div>
 			<Link to='/' className='link'>
 				<LogoutButton />
 			</Link>
+		</div>
 		</div>
 	);
 }
